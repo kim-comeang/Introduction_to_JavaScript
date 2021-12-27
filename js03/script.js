0c4553517e11124e9dc6ab8a754bb084b1aee8d7
kim-comeang <choolooss@naver.com>
2021-12-28 00:11:48 +0900

    변수

---
+++ js03/script.js
@@ -0,0 +1,15 @@
+console.log(123);
+console.log(3.14);
+console.log('문자');
+console.log("문" + "자");
+console.log("@#%&*(");
+console.log("1           2");
+
+let test;
+test = 1000;
+console.log(test);
+
+test = "문자열";
+console.log(test);
+
+
