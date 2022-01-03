30a93ff2de357d32cd7610fc02324e747d3ee479
kim-comeang <choolooss@naver.com>
2022-01-04 01:06:05 +0900

    dom

---
+++ js07/script.js
@@ -0,0 +1,17 @@
+let num;
+
+// 숫자 0
+num = 0;
+console.log(num)
+// 아무것도 없음
+num = null;
+console.log(num)
+
+console.log("------")
+
+let data;
+console.log(data)
+data = "Hello"
+console.log(data)
+data = null
+console.log(data)
