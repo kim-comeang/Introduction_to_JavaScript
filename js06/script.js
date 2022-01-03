5c7cc077dab2b96432eba9ac9afcae450f3b5180
kim-comeang <choolooss@naver.com>
2022-01-04 00:37:04 +0900

    연산, 연산자

---
+++ js06/script.js
@@ -0,0 +1,17 @@
+console.log(3+4);
+console.log(3-4);
+console.log(3*4);
+console.log(3/4);
+console.log(3%4);
+
+
+
+
+
+console.log('00000000000000000000')
+let result = 3
+
+result += 3
+console.log(result)
+result -= 2
+console.log(result)
