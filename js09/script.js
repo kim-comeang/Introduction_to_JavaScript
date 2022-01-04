e87cfdb04d58f360f2ce0fdb186603d906baec69
kim-comeang <choolooss@naver.com>
2022-01-05 01:12:02 +0900

    document & methods

---
+++ js09/script.js
@@ -0,0 +1,22 @@
+console.log(document.querySelector('h1'))
+console.log(document.querySelector('p'))
+console.log(document.querySelector('#text'))
+console.log(document.querySelector('.desc'))
+
+
+
+
+console.log(document.getElementById('text'))
+console.log(document.getElementById('desc'))
+
+
+
+const hh = document.querySelector('h1')
+const pp = document.getElementById('text')
+
+
+console.log(hh.textContent);
+hh.textContent = 'abcd'
+
+pp.textContent = 'z321zz321asd32q1we321'
+console.log(pp.textContent);
