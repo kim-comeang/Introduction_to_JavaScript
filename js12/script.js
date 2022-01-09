bdfa4abd38e1b1835db39aa42d028ab7612e8f78
kim-comeang <choolooss@naver.com>
2022-01-10 02:06:25 +0900

    반복문 while문, for문

---
+++ js12/script.js
@@ -0,0 +1,4 @@
+while(confirm("confirm 메시지")) {
+    console.log("확인")    
+}
+console.log("취소")
---
+++ js13/script.js
@@ -0,0 +1,13 @@
+for(let i = 1; i <= 8; i += 2){
+    console.log("i 반복중");
+    console.log(i);
+
+    // if( i % 2 == 0){
+    //     console.log(i)
+    // }
+};
+
+for(let j = 8; j >= 1; j -= 2){
+    console.log("j 반복중");
+    console.log(j);
+}
